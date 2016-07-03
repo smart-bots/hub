b35c4c5403963bab3b77bd1ff823ea09c89fc6b5
Huỳnh Đức Duy <h2dvnnet@gmail.com>
2016-07-03 18:36:27 +0700

    Core changed to NodeMCU 1.0 (ESP 12E Devkit)

--- code/hub.ino
+++ code/hub.ino
@@ -1,118 +1,43 @@
+#include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 #include <SPI.h>
 #include <RF24.h>
 #include <nRF24L01.h>
-#include <printf.h>
-RF24 radio(7,8); // ce,cs
-#define ssid "Duc Duy Wifi"
-#define password "1tu2duy3"
-#define server "192.168.1.10"
-#define port 80
-#define apiKey "BqkbCjdsOIicxLwQo5CT7e9gBKxbcHM9hHJs4JH19UlnsntCmx"
+
+RF24 radio(4,15); // ce,cs
+
 const uint64_t addr = 0x0a0c0a0c0aLL;
-String uri,packet;
+
+char* ssid = "Duc Duy Wifi";
+char* password = "1tu2duy3";
+char* host = "192.168.1.14"; 
+int port = 80;
+char* hubToken = "Vznx2HNra15IBNEGys8I1xsjm6FTq80v9ZJe8sufj5CVsjOgqK";
+
+String content;
 char down[13],key[11],tus,hard;
-unsigned long time1;
-void start()
-{
-  Serial.println("STARTING...");
-  
-  Serial1.setTimeout(10000);
-
-  while (!Serial1.availableForWrite()) {}
-  Serial1.println("AT");
-  if (Serial1.find("OK")) {
-    Serial.println("AT OK");
-  } else {
-    Serial.println("AT TIMED OUT");
-  }
-
-  while (!Serial1.availableForWrite()) {}
-  Serial1.println("AT+RST");
-//  if (Serial1.find("ready")) {
-  if (Serial1.find("invalid")) {
-    Serial.println("RST OK");
-  } else {
-    Serial.println("RST TIMED OUT");
-  }
-
-  boolean wifiCheck = false;
-  if (Serial1.find("WIFI GOT IP")) {
-    Serial.println("WIFI CONNECTED AND GOT IP");
-    wifiCheck = true;
-  }
-
-  while (!Serial1.availableForWrite()) {}
-  Serial1.println("AT+CWMODE?");
-  if (Serial1.find("+CWMODE:1")) {
-    Serial.println("AT CWMODE 1[1] OK");
-  } else {
-      while (!Serial1.availableForWrite()) {}
-      Serial1.println("AT+CWMODE=1");
-    if (Serial1.find("OK")) {
-      Serial.println("AT CWMODE 1[2] OK");
-    } else {
-      Serial.println("AT CWMODE 1 TIMED OUT");
-    }
-  }
-  if (!wifiCheck) {
-    while (!Serial1.availableForWrite()) {}
-    Serial1.println("AT+CWJAP?");
-    if (Serial1.find(ssid)) {
-      Serial.println("CWJAP[1] OK");
-    } else {
-      while (!Serial1.availableForWrite()) {}
-      Serial1.println("AT+CWJAP=\"" + String(ssid) + "\",\"" + String(password) + "\"");
-      if (Serial1.find("OK")) {
-        Serial.println("CWJAP[2] OK");
-      } else {
-        Serial.println("CWJAP TIMED OUT");
-      }
-    }
-  } else {
-    Serial.println("NO NEED TO CWJAP");
-  }
-
-  while (!Serial1.availableForWrite()) {}
-  Serial1.println("AT+CIPMUX?");
-  if (Serial1.find("+CIPMUX:0")) {
-    Serial.println("CIPMUX 0[1] OK");
-  } else {
-    while (!Serial1.availableForWrite()) {}
-    Serial1.println("AT+CIPMUX=0");
-    if (Serial1.find("OK")) {
-      Serial.println("CIPMUX 0[2] OK");
-    } else {
-      Serial.println("CIPMUX TIMED OUT");
-    }
-  }
-
-  while (!Serial1.availableForWrite()) {}
-  Serial1.println("AT+CIPSTART=\"TCP\",\"" + String(server) + "\"," + String(port));
-  if (Serial1.find("OK")) {
-    Serial.println("CIPSTART OK");
-  } else {
-    Serial.println("CIPSTART TIMED OUT");
-  }
-
-  Serial1.setTimeout(1000);
-}
 
 void setup() {
   Serial.begin(115200);
-  Serial1.begin(115200);
+
   radio.begin();
-//   printf_begin();
-//   radio.printDetails();
   radio.openWritingPipe(addr);
   radio.openReadingPipe(1,addr);
   radio.startListening();
-  while (!Serial1) {}
-  start();
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println(".");
+  }
+
+  Serial.println("WiFi connected");  
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-
   if (radio.available()) {
     memset(down,' ',sizeof(down));
     radio.read(down,sizeof(down));
@@ -121,84 +46,103 @@ void loop() {
     strncpy(key,down,10);
     tus = down[10];
     hard = down[11];
-    uri     = "/smartbots/api/" + String(apiKey) + "/up/" + String(key) + "/" + String(tus) + "/" + String(hard);
-    packet  = "GET " + uri + " HTTP/1.1\r\nHost: " + server + "\r\n\r\n";
-    while (!Serial1.availableForWrite()) {}
-    Serial1.print("AT+CIPSEND=");
-    Serial1.println(packet.length());
-    
-    if (Serial1.find(">")) {
-      Serial.println("CIPSEND OK");
-      while (!Serial1.availableForWrite()) {}
-      Serial1.print(packet);
-      if (Serial1.find("SEND OK\r\n")) {
-        Serial.println("CIPSEND DONE");
-      } else {
-        Serial.println("CIPSEND[2] TIMED OUT");
-        start();
-      }
-    } else {
-      Serial.println("CIPSEND[1] TIMED OUT");
-      start();
-    }
-    
-  } else {
 
-    if ((unsigned long)(millis()-time1) > 500)
-    {
-      uri     = "/smartbots/api/" + String(apiKey) + "/down";
-      packet  = "GET " + uri + " HTTP/1.1\r\nHost: " + server + "\r\n\r\n";
-      
-      while (!Serial1.availableForWrite()) {}
-      Serial1.print("AT+CIPSEND=");
-      Serial1.println(packet.length());
-      
-      if (Serial1.find(">")) {
-        Serial.println("CIPSEND OK");
-        while (!Serial1.availableForWrite()) {}
-        Serial1.print(packet);
-        
-        if (Serial1.find("SEND OK\r\n")) {
-      
-          Serial.println("CIPSEND DONE");
-        
-          if (Serial1.find("\r\n\r\n")) {
-            unsigned int i = 0;
-            unsigned int j = 0;
-            char outputString[100] = {};
-            while (i < 5000) { // Kiểm tra thời gian
-              if (Serial1.available()) {
-                outputString[j] = Serial1.read();
-                i = 0; // Đặt lại bộ đếm thời gian
-                j++;
-              }
-              i++;
-            }
-            StaticJsonBuffer<200> jsonBuffer;
-            JsonObject& data = jsonBuffer.parseObject(outputString);
-            Serial.print("DATA FROM ESP: ");
-            data.printTo(Serial);
-            Serial.println();
-            for (int i = 0; i < data["c"]; i++) {
-              char y[] = {};
-              String str;
-              str=String(i);
-              str.toCharArray(y,10);
-              const char* up = data[y];
-              radio.stopListening();
-              radio.write(up,11);
-              radio.startListening();
-            }
-          }
-        } else {
-          Serial.println("CIPSEND[2] TIMED OUT");
-          start();
-        }
-      } else {
-        Serial.println("CIPSEND[1] TIMED OUT");
-        start();
+    WiFiClient client;
+    client.stop();
+
+    if (!client.connect(host, port)) {
+      Serial.print("Connect to ");
+      Serial.print(host);
+      Serial.println(" failed");
+      return;
+    } else {
+      Serial.print("Connected to ");
+      Serial.println(host);
+    }
+
+    String url = "/smartbots/api/";
+    url += hubToken;
+    url += "/up/";
+    url += key;
+    url += "/";
+    url += tus;
+    url += "/";
+    url += hard;
+  
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+  
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" + 
+                 "Connection: close\r\n\r\n");
+    
+    unsigned long timeout = millis();
+    
+    while (client.available() == 0) {
+      if (millis() - timeout > 5000) {
+        Serial.println(">>> Client Timeout <<<");
+        client.stop();
+        return;
       }
-      time1 = millis();
     }
   }
+
+  WiFiClient client;
+  client.stop();
+
+  if (!client.connect(host, port)) {
+    Serial.print("Connect to ");
+    Serial.print(host);
+    Serial.println(" failed");
+    return;
+  } else {
+    Serial.print("Connected to ");
+    Serial.println(host);
+  }
+
+  String url = "/smartbots/api/";
+  url += hubToken;
+  url += "/down";
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  
+  unsigned long timeout = millis();
+  
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout <<<");
+      client.stop();
+      return;
+    }
+  }
+
+  if (client.find("\r\n\r\n")) {
+    while (client.available()){
+      content = client.readStringUntil('\n');
+    }
+  }
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& data = jsonBuffer.parseObject(content);
+  Serial.print("DATA FROM ESP: ");
+  data.printTo(Serial);
+  Serial.println();
+
+  for (int i = 0; i < data["c"]; i++) {
+    char y[] = {};
+    String str;
+    str=String(i);
+    str.toCharArray(y,10);
+    const char* up = data[y];
+    radio.stopListening();
+    radio.write(up,11);
+    radio.startListening();
+  }
+
 }
+
