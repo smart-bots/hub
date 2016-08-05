d78a1b2a53d7e217ce9ff48432ec494cacea8adf
Huỳnh Đức Duy <h2dvnnet@gmail.com>
2016-08-05 15:32:43 +0700

    #8

--- code/hub.ino
+++ code/hub.ino
@@ -9,7 +9,7 @@
 #include <Wire.h>
 
 const int maxRetries = 20;
-char* host = "192.168.1.11";
+char* host = "192.168.1.15";
 int port = 80;
 const char* ssid = "DemoHub";
 const char* password = "demopass";
@@ -144,7 +144,8 @@ void setup() {
     lcd.print("|");
     lcd.print(password);
     lcd.setCursor(0,1);
-    lcd.print(WiFi.softAPIP());
+//    lcd.print(WiFi.softAPIP());
+    lcd.print("192.168.4.1");
     goAP = true;
   }
 
@@ -204,7 +205,7 @@ void startAP(void) {
   WiFi.softAP(ssid, password);
 
   server.on("/", []() {
-      content = "<!DOCTYPE html><meta charset=UTF-8><meta content='ie=edge' http-equiv='x-ua-compatible'><meta content='width=device-width, initial-scale=1.0' name='viewport'><title>Setting up your hub</title><style>*{-moz-box-sizing:border-box;box-sizing:border-box}body{font:16px Helvetica;background:#36404a}section{width:275px;background:#ecf0f1;padding:0 30px 30px 30px;margin:60px auto;text-align:center;border-radius:5px}span{display:block;position:relative;margin:0 auto;top:-40px;height:80px;width:80px;font-size:70px;border-radius:50%;background-color:#5fbeaa;color:#fff;box-shadow:1px 1px 2px rgba(0,0,0,.3)}h1{font-size:24px;font-weight:100;margin-bottom:30px;margin-top:-10px}input{width:100%;background:#bdc3c7;border:none;height:30px;margin-bottom:15px;border-radius:5px;text-align:center;font-size:14px;color:#7f8c8d}input:focus{outline:0}button{width:100%;height:30px;border:none;background:#5fbeaa;color:#ecf0f1;font-weight:100;margin-bottom:15px;border-radius:5px;transition:all ease-in-out .2s;border-bottom:3px solid #56a695}button:focus{outline:0}button:hover{background:#56a695}</style><section><span>+</span><h1>Hub Setup</h1><form action=setup><input name=ssid placeholder=SSID><input name=password placeholder=Password type=password><input name=token placeholder=Token><button type=submit>Setup</button></form></section>";
+      content = "<!DOCTYPE html><meta charset='utf-8'><meta content='ie=edge' http-equiv='x-ua-compatible'><meta content='width=device-width, initial-scale=1.0' name='viewport'><title>Setting up your hub</title><style>*{-moz-box-sizing:border-box;box-sizing:border-box}body{font:16px Helvetica;background:#36404a}section{width:275px;background:#ecf0f1;padding:0 30px 30px 30px;margin:60px auto;text-align:center;border-radius:5px}span{display:block;position:relative;margin:0 auto;top:-40px;height:80px;width:80px;font-size:70px;border-radius:50%;background-color:#5fbeaa;color:#fff;box-shadow:1px 1px 2px rgba(0,0,0,.3)}h1{font-size:24px;font-weight:100;margin-bottom:30px;margin-top:-10px}input{width:100%;background:#bdc3c7;border:none;height:30px;margin-bottom:15px;border-radius:5px;text-align:center;font-size:14px;color:#7f8c8d}input:focus{outline:0}button{width:100%;height:30px;border:none;background:#5fbeaa;color:#ecf0f1;font-weight:100;margin-bottom:15px;border-radius:5px;transition:all ease-in-out .2s;border-bottom:3px solid #56a695}button:focus{outline:0}button:hover{background:#56a695}</style><body><script>function setup(){var xhttp=new XMLHttpRequest(); xhttp.onreadystatechange=function(){if (xhttp.readyState==4 && xhttp.status==200){alert(xhttp.responseText);}}; xhttp.open('GET', 'setup?ssid='+document.getElementsByName('ssid')[0].value+'&password='+document.getElementsByName('password')[0].value+'&token='+document.getElementsByName('token')[0].value, true); xhttp.send(); return false;}</script><section> <span>+</span> <h1>Hub Setup</h1> <form id='hubSetup'> <input name='ssid' placeholder='SSID'> <input name='password' placeholder='Password' type='password'> <input name='token' placeholder='Token'> <button type='submit' onclick='return setup();'>Setup</button> </form></section></body></html>";
       server.send(200, "text/html", content);  
   });
     
@@ -235,11 +236,11 @@ void startAP(void) {
         }
 
         EEPROM.commit();
-        server.send(200, "application/json", "{\"Success\":\"true\"}");
+        server.send(200, "text/html", "Hub setup successfully!");
         ESP.restart();
 
       } else {
-        server.send(200, "application/json", "{\"Success\":\"false\"}");
+        server.send(200, "text/html", "Please re-check infomation!");
       }
       
   });
